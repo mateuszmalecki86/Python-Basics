58fab9b08c362812eee52b07d89c63811a8e916d
mateuszmalecki86 <mateuszmalecki86@gmail.com>
2019-09-23 14:51:19 +0200

    Test

---
+++ Lab03B-Strings.py
@@ -0,0 +1,33 @@
+print("This is a line of test!")
+
+print("This is a first line!\nThis is a second line!")
+
+
+print("This is some\" text")
+
+
+phrase = "String in a variable"
+print(phrase)
+print("\n" + phrase + " and concatenated string")
+
+print(phrase.capitalize())
+
+print(phrase.upper())
+
+print(phrase.isupper())
+
+print(phrase.islower())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+print(phrase[2])
+
+print("\n")
+print(phrase.index("S"))
+print(phrase.index("i"))
+
+
+print(phrase)
+
+print(phrase.replace("variable", "variable_replaced"))
