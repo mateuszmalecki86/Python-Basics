04ef8121d466ea7973d970b8e3af7b865bdb869e
mateuszmalecki86 <mateuszmalecki86@gmail.com>
2019-09-25 09:11:31 +0200

    Test

---
+++ Lab03W-Writing_to_files.py
@@ -0,0 +1,17 @@
+
+# employees_var = open("employees.txt", "a")
+
+
+# employees_var.write("\nLukasz - Tower Head")
+
+# employees_var.write("\nKelly - HR")
+
+# employees_var.close
+
+employees_var = open("employees.txt", "w")
+
+employees_var.write("\nKelly - HR")
+
+employees_var.close
+
+
